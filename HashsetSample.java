a566a42aefa03affc34ed91f6ca3f3d69eccb19e
Ryan Joel <ryanjoelimba@gmail.com>
2022-11-07 22:49:36 +0800

    modifuy the remove function

--- HashsetSample.java
+++ HashsetSample.java
@@ -21,34 +21,53 @@ public class HashsetSample{
 
 
 		switch(choice){
-		case 1:
+		case 1://add item in hashmap
 			System.out.println("Enter ID number:");
 			String id = sc.nextLine();
+			if(id.length() > 9){
+				System.out.println("ERROR! ID input overlapse");
+				break;
+			}
+
 			System.out.println("Enter Name:");
 			String name = sc.nextLine();
 
 			student.put(id,name);
 		break;
 
-		case 2:
+		case 2://remove item in hashmap
 			System.out.println("Enter Id Number: ");
 			String removeID = sc.nextLine();
 
-			student.remove(removeID);
-		break;
 
-		case 3:
+
 			for(String i : student.keySet()){
-				System.out.println("ID Number: "+ i + " ______" + "Name: " + student.get(i));
+
+				
+				if(i.equals(removeID)) { // check if id number is in the hashmap
+					student.remove(removeID);
+					System.out.println(removeID + " has been successfully deleted!");
+					break;
+				}
+				else{
+					System.out.println("Cannot find ID");
+				}
+			
 			}
 		break;
 
-		case 4:
+		case 3: // display items in the hashmap
+			for(String i : student.keySet()){
+				System.out.println("ID Number: "+ i + " ______ " + "Name: " + student.get(i));
+			}
+		break;
+
+		case 4: //exits the loop
 			System.out.println("Thank you for using the app!");
 			System.exit(1);
 
 		break;
-		default:
+		default://return error message if input is invalid
 			System.out.println("Invalid input!");
 		break;
 
